d268d7ee3eecc91f2f48a78508ff5d1dd78886c5
kjstanfield <kjstan@gmail.com>
2020-09-05 00:06:11 -0400

    Updated temporary logos.

--- src/components/Rat.js
+++ src/components/Rat.js
@@ -1,10 +1,27 @@
-import React from "react";
-export default function Rat({ name, server, character }) {
-  return (
-    <div className="sewer-rat">
-      <h2>{name}</h2>
-      <h3>{server}</h3>
-      <h3>{character}</h3>
-    </div>
-  );
+import React from "react"
+
+// const Rat = ({ rat }) => {
+//     return (
+//         <div className="rat">
+//             <h2>{name}</h2>
+//             <div className="rat-stats">
+//                 <div>{rat.race.name}</div>
+//                 <div>{rat.character_class.name}</div>
+//                 <div>{rat.realm.name}</div>
+//                 <div>{rat.level}</div>
+//                 <div>{rat.experience}</div>
+//                 <div>{rat.average_item_level}</div>
+//             </div>
+//         </div>
+//     )
+// }
+
+const Rat = () => {
+    return (
+        <div className="rat">
+            <h2>Hello World!</h2>
+        </div>
+    )
 }
+
+export default Rat
