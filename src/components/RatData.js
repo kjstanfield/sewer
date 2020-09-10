c347414ad11d798e96bb25194cbd5bcc87e0c1dd
Todd Culver <tawddd@gmail.com>
2020-09-10 19:18:30 -0400

    Added more rats to RatData.js

--- src/components/RatData.js
+++ src/components/RatData.js
@@ -21,11 +21,39 @@ const RatList = [
     character: "bighamspankr",
   },
   {
-    name: "Pizzasupper",
-    rname: "Todd",
+    name: "Ben",
+    rname: "Ben",
     server: "Area 52",
     slug: "area-52",
-    character: "miseryclub",
+    character: "daporksword",
+  },
+  {
+    name: "tomb",
+    rname: "Tom",
+    server: "Area 52",
+    slug: "area-52",
+    character: "bennet",
+  },
+  {
+    name: "Cashews",
+    rname: "Pete",
+    server: "Area 52",
+    slug: "area-52",
+    character: "moldman",
+  },
+  {
+    name: "Muenster",
+    rname: "Dalton",
+    server: "Tichondrius",
+    slug: "tichondrius",
+    character: "hellim",
+  },
+  {
+    name: "Drlol",
+    rname: "Cody",
+    server: "Mug'thol",
+    slug: "mugthol",
+    character: "fluentx",
   },
 ];
 
